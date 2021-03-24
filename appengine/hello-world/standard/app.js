ff222ce81c902a05a06c23f4f74948690dfe44c3
alexbarsan944 <39854976+alexbarsan944@users.noreply.github.com>
2021-03-24 18:49:51 +0200

    auth

--- appengine/hello-world/standard/app.js
+++ appengine/hello-world/standard/app.js
@@ -1,27 +1,62 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
-
-// [START gae_node_request_example]
 const express = require('express');
+const metadata = require('gcp-metadata');
+const {OAuth2Client} = require('google-auth-library');
 
 const app = express();
+const oAuth2Client = new OAuth2Client();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+// Cache externally fetched information for future invocations
+let aud;
+
+async function audience() {
+  if (!aud && (await metadata.isAvailable())) {
+    let project_number = await metadata.project('203351277928');
+    let project_id = await metadata.project('cloudcomputin2021');
+
+    aud = '/projects/' + project_number + '/apps/' + project_id;
+  }
+
+  return aud;
+}
+
+async function validateAssertion(assertion) {
+  if (!assertion) {
+    return {};
+  }
+
+  // Check that the assertion's audience matches ours
+  const aud = await audience();
+
+  // Fetch the current certificates and verify the signature on the assertion
+  const response = await oAuth2Client.getIapPublicKeys();
+  const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
+      assertion,
+      response.pubkeys,
+      aud,
+      ['https://cloud.google.com/iap']
+  );
+  const payload = ticket.getPayload();
+
+  // Return the two relevant pieces of information
+  return {
+    email: payload.email,
+    sub: payload.sub,
+  };
+}
+
+app.get('/', async (req, res) => {
+  const assertion = req.header('X-Goog-IAP-JWT-Assertion');
+  let email = 'None';
+  try {
+    const info = await validateAssertion(assertion);
+    email = info.email;
+    console.log(info.sub)
+  } catch (error) {
+    console.log(error);
+  }
+  res.status(200).send(`Hello ${email}`).end();
 });
+
 
 // Start the server
 const PORT = process.env.PORT || 8080;
@@ -29,6 +64,3 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
-// [END gae_node_request_example]
-
-module.exports = app;
